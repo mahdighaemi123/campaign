57550fc509af87c414c8a7470d750fa9ad984b8a
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2025-08-14 12:53:52 +0330

    50

--- ai/bot.py
+++ ai/bot.py
@@ -706,7 +706,7 @@ class FAQBot:
 
             if status == "hi":
                 await self.message_sender.send_stored_message(
-                    "689ccd6485a5fe90160f818d",
+                    "689dab0bb7b7ce7565a4b28e",
                     chat_id,
                     business_connection_id,
                     status_ai_data
